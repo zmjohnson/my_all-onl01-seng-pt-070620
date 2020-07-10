46fb29e23351d026788d1ab0c09ee34422369a8d
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-07-10 19:42:36 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,9 +2,11 @@ require 'pry'
 
 def my_all?(collection)
   i = 0
+  block_return_values = []
   while i < collection.length
+      block_return_values <<
     yield(collection[i])
-    i = i + 1
+     i = i + 1
   end
 
 binding.pry
